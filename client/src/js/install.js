bd97f49d1d2893527d7603db3b81b872e2b8c1b2
Bro74 <bmwhite1014@gmail.com>
2023-05-31 13:25:13 -0400

    boiler plate, event listener

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,9 +3,24 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {});
-
+    event.preventDefault();
+    const defferedPrompt = event; 
+        //...
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (defferedPrompt) {
+        defferedPrompt.prompt();
+        const choiceResult = await deferredPrompt.userchoice;
+        if (choiceResult.outcome == 'acceptted'){
+            console.log('User accepted installation');
+        }else {
+            console.log('User dismissed installation');
+        }
+        defferedPrompt = null;
+    }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('App installed succesfully');
+});
